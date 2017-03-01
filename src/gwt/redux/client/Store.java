98da694978c797a57a827d1c58f93818bdc6f579
Paul Stockley <pstockley1@gmail.com>
2017-03-01 00:20:50 -0500

    Update to support ES6 Style components

--- src/gwt/redux/client/Store.java
+++ src/gwt/redux/client/Store.java
@@ -8,5 +8,5 @@ public class Store<STATE> {
     public native <A extends Action> void dispatch(A theAction);
     public native STATE getState();
     public native Unsubscribe subscribe(Listener listener);
-    public native void replaceReducer(Reducer nextReducer);
+    public native void replaceReducer(Reducer<?,?> nextReducer);
 }
--- src/gwt/redux/client/addons/react_redux/ReactRedux.java
+++ src/gwt/redux/client/addons/react_redux/ReactRedux.java
@@ -1,14 +1,14 @@
 package gwt.redux.client.addons.react_redux;
 
-import gwt.react.client.components.ReactClass;
+import gwt.react.client.components.ComponentConstructorFn;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "ReactRedux")
 public class ReactRedux {
-    public static ReactClass<ProviderProps> Provider;
+    public static ComponentConstructorFn<ProviderProps> Provider;
 
     public static native ConnectToComponentFn connect();
-    public static native ConnectToComponentFn connect(MapStateToPropsFn mapStateToProps);
-    public static native ConnectToComponentFn connect(MapStateToPropsFn mapStateToProps, MapDispatchToPropsFn mapDispatchToProps);
+    public static native ConnectToComponentFn connect(MapStateToPropsFn<?,?> mapStateToProps);
+    public static native ConnectToComponentFn connect(MapStateToPropsFn<?,?> mapStateToProps, MapDispatchToPropsFn<?> mapDispatchToProps);
 }
